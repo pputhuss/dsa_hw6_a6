1a4d529afa0eb02dbd6d89cd3efb5287c48469af
Prathik Praveen Puthussery <pputhuss@eceprog3.ecn.purdue.edu>
2024-11-04 21:40:32 -0500

    trying step 3 but failing

--- a6.c
+++ a6.c
@@ -8,6 +8,8 @@ typedef struct Point {
     int label;    
     int x;
     int y;
+    int x_point;
+    int y_point;
     struct Point *left;
     struct Point *right;
 } Point;
@@ -75,9 +77,11 @@ void new_Block(int label, int x_val, int y_val, int iteration) {
     newPoint->label = label;
     newPoint->x = x_val;
     newPoint->y = y_val;
+    newPoint->x_point = -1;
+    newPoint->y_point = -1;    
     newPoint->left = NULL;
     newPoint->right = NULL;
-    if (iteration == 1) {
+    if (iteration == 1 || iteration == 3) {
         add_To_Tree(newPoint);
     }
     if (iteration == 2) {    
@@ -91,6 +95,8 @@ void new_VH(char label, int iteration) {
     //using -1 to show that it is not an actual dimension
     newPoint->x = -1; 
     newPoint->y = -1; 
+    newPoint->x_point = -1;
+    newPoint->y_point = -1;   
     newPoint->left = NULL;
     newPoint->right = NULL;
     if (iteration == 1) {
@@ -127,6 +133,11 @@ void dim_list(int label, int x_val, int y_val) {
     }
 }
 
+void get_coords(Point* root, int x_curr, int y_curr) //starts at 0,0 for root btw
+{
+
+}
+
 int main(int argc, char* argv[])
 {    
     char filename[] = "input.txt"; //i just put input.txt for now
@@ -141,27 +152,29 @@ int main(int argc, char* argv[])
     char label_VH;
     int x_val = 0; //block x dim
     int y_val = 0; //block y dim
-    int iteration = 2;
+    int iteration = 3;
 
     //PART 1
-    while (!feof(file)) {
-        if (fscanf(file, "%d(%d,%d)\n", &label, &x_val, &y_val) == 3) {
-            new_Block(label, x_val, y_val, iteration);
-        } 
-        else if (fscanf(file, "%c\n", &label_VH) == 1) {
-            new_VH(label_VH, iteration);
-        } 
-        else 
-        {
-            //if neither format matched
-            break;
+    if (iteration == 1) {
+        while (!feof(file)) {
+            if (fscanf(file, "%d(%d,%d)\n", &label, &x_val, &y_val) == 3) {
+                new_Block(label, x_val, y_val, iteration);
+            } 
+            else if (fscanf(file, "%c\n", &label_VH) == 1) {
+                new_VH(label_VH, iteration);
+            } 
+            else 
+            {
+                //if neither format matched
+                break;
+            }
         }
-    }
 
-    if ((arr_index > 0) && (iteration == 1)) {
-        //start printing from the last added element (root of the tree)
-        //doing -1 cuz i did i++ at end of each iteration in add_to_tree
-        print_Tree((Point*)arr[arr_index - 1]);  // Start printing from the last added element (root of the tree)
+        if ((arr_index > 0) && (iteration == 1)) {
+            //start printing from the last added element (root of the tree)
+            //doing -1 cuz i did i++ at end of each iteration in add_to_tree
+            print_Tree((Point*)arr[arr_index - 1]);  // Start printing from the last added element (root of the tree)
+        }
     }
     //END OF PART 1
 
@@ -169,37 +182,64 @@ int main(int argc, char* argv[])
     //iteration++; //now it is iteration 2, for part 2 basically
 
     //using this while/file open to find dimensions of V/H nodes
-    rewind(file);
-    while (!feof(file)) {
-        if (fscanf(file, "%d(%d,%d)\n", &label, &x_val, &y_val) == 3) {
-            new_Block(label, x_val, y_val, iteration);
-            dim_list(label, x_val, y_val);
-        } 
-        else if (fscanf(file, "%c\n", &label_VH) == 1) {
-            new_VH(label_VH, iteration);
-
-            //find the most recently processed VH node with this label
-            Point* latest_vh = NULL;
-            for (int j = arr_index2 - 1; j >= 0; j--) {  //search backwards
-                if (arr[j]->label == label_VH) {
-                    latest_vh = arr[j];
-                    break;  //take only the most recent one
+    else if (iteration == 2) {
+        rewind(file);
+        while (!feof(file)) {
+            if (fscanf(file, "%d(%d,%d)\n", &label, &x_val, &y_val) == 3) {
+                new_Block(label, x_val, y_val, iteration);
+                dim_list(label, x_val, y_val);
+            } 
+            else if (fscanf(file, "%c\n", &label_VH) == 1) {
+                new_VH(label_VH, iteration);
+                //find the most recently processed VH node with this label
+                Point* latest_vh = NULL;
+                for (int j = arr_index2 - 1; j >= 0; j--) {  //search backwards
+                    if (arr[j]->label == label_VH) {
+                        latest_vh = arr[j];
+                        break;  //take only the most recent one
+                    }
                 }
+                
+                if (latest_vh != NULL) {
+                    dim_list(latest_vh->label, latest_vh->x, latest_vh->y);
+                    latest_vh->label = 0;  //clearing it so it skips this one next time
+                }
+            } 
+            else 
+            {
+                //if neither format matched
+                break;
             }
-            
-            if (latest_vh != NULL) {
-                dim_list(latest_vh->label, latest_vh->x, latest_vh->y);
-                latest_vh->label = 0;  //clearing it so it skips this one next time
-            }
-        } 
-        else 
-        {
-            //if neither format matched
-            break;
+        }
+        if ((arr_index > 0) && (iteration == 1)) {
+            //start printing from the last added element (root of the tree)
+            //doing -1 cuz i did i++ at end of each iteration in add_to_tree
+            print_Tree((Point*)arr[arr_index - 1]);  // Start printing from the last added element (root of the tree)
         }
     }
     
-    fclose(file);
+    //PART 3
+    else if (iteration == 3) {
+        rewind(file);
+        while (!feof(file)) {
+            if (fscanf(file, "%d(%d,%d)\n", &label, &x_val, &y_val) == 3) {
+                new_Block(label, x_val, y_val, iteration);
+            } 
+            else if (fscanf(file, "%c\n", &label_VH) == 1) {
+                new_VH(label_VH, iteration);
+            } 
+            
+            else 
+            {
+                //if neither format matched
+                break;
+            }
+        }
+    }
 
+    else 
+    {
+        fclose(file);
+    }
     return 0;
 }
