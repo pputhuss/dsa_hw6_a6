b9bab32b58c718edc01a487d18efb37b5f78a9aa
Prathik Praveen Puthussery <pputhuss@eceprog4.ecn.purdue.edu>
2024-11-05 19:56:55 -0500

    trynna submit

--- Makefile
+++ Makefile
@@ -1,8 +1,8 @@
 CC = gcc
 CFLAGS = -O3 -std=c99 -Wall -Wshadow -Wvla -pedantic
 
-a6: main.c
-	$(CC) $(CFLAGS) main.c -o a6
+a6: a6.c
+	$(CC) $(CFLAGS) a6.c -o a6
 
 clean:
 	rm -f a6 out_file1 out_file2 out_file3
--- a6.c
+++ a6.c
@@ -67,11 +67,9 @@ void add_To_Tree_P3(Point* newPoint) {
     }
 }
 
-
 void find_VHdim(Point* newPoint) {
     if (arr_index2 < SIZE) 
     {
-        //arr defined in global var section
         arr[arr_index2] = newPoint;
 
         if ((newPoint->label == 'V' || newPoint->label == 'H') && (arr_index2 >= 2)) {
@@ -154,197 +152,190 @@ void new_VH(char label, int iteration) {
     }
 }
 
-
-void print_Tree(Point* root) {
+void print_Tree_To_File(Point* root, FILE* outfile) {
     if (root == NULL) {
         return;
     }
     
     if (root->label == 'V' || root->label == 'H') {
-        printf("%c\n", (char)root->label);
+        fprintf(outfile, "%c\n", (char)root->label);
     }
     else {
-        printf("%d(%d,%d)\n", root->label, root->x, root->y);
+        fprintf(outfile, "%d(%d,%d)\n", root->label, root->x, root->y);
     }
-    print_Tree(root->left);
-    print_Tree(root->right);
+    print_Tree_To_File(root->left, outfile);
+    print_Tree_To_File(root->right, outfile);
 }
 
-void dim_list(int label, int x_val, int y_val) {
+void dim_list_To_File(int label, int x_val, int y_val, FILE* outfile) {
     if (label == 'V' || label == 'H') {
-        printf("%c(%d,%d)\n", label, x_val, y_val);
+        fprintf(outfile, "%c(%d,%d)\n", label, x_val, y_val);
     }
     else {
-        printf("%d(%d,%d)\n", label, x_val, y_val);
+        fprintf(outfile, "%d(%d,%d)\n", label, x_val, y_val);
     }
 }
 
 //global var just for this function
-int right_sub_iteration = 0;
+// int right_sub_iteration = 0;
+// int packaging_right_side(Point* root, int x_curr, int y_curr) //starts at 0,0 and root btw
+// {
+//     //first need to get to bottommost V/H node on right subtree
+//     //this if is used to make it go right first from node
+//     if (right_sub_iteration == 0)
+//     {
+//         right_sub_iteration = 1;
+//         packaging_right_side(root->right, x_curr, y_curr);
+//     }
+//     else
+//     {
+//         if ((root->right)->label == 'V' || (root->right)->label == 'H') 
+//         {
+//             packaging_right_side(root->right, x_curr, y_curr);
+//         }
+//         else if ((root->left)->label == 'V' || (root->left->label) == 'H')
+//         {
+//             packaging_right_side(root->left, x_curr, y_curr);
+//         }
 
-int packaging_right_side(Point* root, int x_curr, int y_curr) //starts at 0,0 and root btw
-{
-    //first need to get to bottommost V/H node on right subtree
-    //this if is used to make it go right first from node
-    if (right_sub_iteration == 0)
-    {
-        packaging_right_side(root->right, 0, 0);
-        right_sub_iteration = 1;
-    }
-    else
-    {
-        if ((root->right)->label == 'V' || (root->right)->label == 'H') 
-        {
-            packaging_right_side(root->right, 0, 0);
-        }
-        else if ((root->left)->label == 'V' || (root->left->label) == 'H')
-        {
-            packaging_right_side(root->left, 0, 0);
-        }
+//         else //if it reaches here the first iteration will be bottommost V/H
+//         {
+//             //setting bottomost V/H's left node to (0,0)
+//             (root->left)->x_point = x_curr; 
+//             (root->left)->y_point = y_curr;
 
-        else //if it reaches here the first iteration will be bottommost V/H
-        {
-            //setting bottomost V/H's left node to (0,0)
-            (root->left)->x_point = x_curr; 
-            (root->left)->y_point = y_curr;
+//             if(root->label == 'V') 
+//             {
+//                 root->x_point = (root->left)->x_point;
+//                 root->y_point = (root->left)->y_point;
 
-            if(root->label == 'V') 
-            {
-                (root->right)->x_point = (root->left)->x + (root->left)->x_point;
+//                 //root->right and root x val updates
+//                 (root->right)->x_point = (root->left)->x; //+ (root->left)->x_point;
+//                 root->x = (root->left)->x + (root->right)->x; //V's x dim would be leftx + rightx
                 
-                //i think the right node will always have the same y for a V cut as left node
-                (root->right)->y_point = (root->left)->y;
-                // if ((root->right)->y > (root->left)->y) {
-                //     (root->right)->y_point = (root->right)->y;
-                // }
-                // else 
-                // {
-                //     (root->right)->y_point = (root->left)->y;
+//                 //root->right and root y val updates
+//                 //i think the right node will always have the same y for a V cut as left node
+//                 (root->right)->y_point = (root->left)->y_point;
 
-                // }
-            }
+//                 //V's y dim would be the highest of the left and right node 
+//                 if ((root->right)->y > (root->left)->y) {
+//                     root->y = (root->right)->y;
+//                 }
+//                 else 
+//                 {
+//                     (root)->y = (root->left)->y;
+//                 } //at this point i have gotten the dimensions of V node and set both child nodes
+//                 return;
 
-        }
-    }
-}
+//             }
+
+//         }
+//     }
+// }
 
 int main(int argc, char* argv[])
 {    
-    char filename[] = "input.txt"; //i just put input.txt for now
-
-    FILE* file = fopen(filename, "r");
-    if (file == NULL) {
-        printf("Could not open file %s\n", filename);
-        return -1; //did not work
+    //check command line arguments
+    if (argc != 5) {
+        printf("nah");
+        return EXIT_FAILURE;
     }
 
-    int label = 0; //number label
+    //open input and output files
+    FILE* in_file = fopen(argv[1], "r");
+    FILE* out_file1 = fopen(argv[2], "w");
+    FILE* out_file2 = fopen(argv[3], "w");
+    FILE* out_file3 = fopen(argv[4], "w");
+
+    if (in_file == NULL || out_file1 == NULL || out_file2 == NULL || out_file3 == NULL) {
+        fprintf(stderr, "error opening files\n");
+        return EXIT_FAILURE;
+    }
+
+    int label = 0;
     char label_VH;
-    int x_val = 0; //block x dim
-    int y_val = 0; //block y dim
-    int iteration = 3;
+    int x_val = 0;
+    int y_val = 0;
+    int iteration = 1; //start with part 1
 
     //PART 1
-    if (iteration == 1) {
-        while (1) {
-            if (fscanf(file, "%d(%d,%d)\n", &label, &x_val, &y_val) == 3) {
-                new_Block(label, x_val, y_val, iteration);
-            } 
-            else if (fscanf(file, "%c\n", &label_VH) == 1) {
-                new_VH(label_VH, iteration);
-            } 
-            else 
-            {
-                //if neither format matched
-                break;
-            }
-        }
-
-        if ((arr_index > 0) && (iteration == 1)) {
-            //start printing from the last added element (root of the tree)
-            //doing -1 cuz i did i++ at end of each iteration in add_to_tree
-            print_Tree((Point*)arr[arr_index - 1]);  // Start printing from the last added element (root of the tree)
+    while (1) {
+        if (fscanf(in_file, "%d(%d,%d)\n", &label, &x_val, &y_val) == 3) {
+            new_Block(label, x_val, y_val, iteration);
+        } 
+        else if (fscanf(in_file, "%c\n", &label_VH) == 1) {
+            new_VH(label_VH, iteration);
+        } 
+        else {
+            break;
         }
     }
-    //END OF PART 1
+
+    //pre-order traversal to out_file1
+    print_Tree_To_File(arr[arr_index - 1], out_file1);
 
     //PART 2
-    //iteration++; //now it is iteration 2, for part 2 basically
+    rewind(in_file); //geeks for geeks, lets me reread from beginning
+    iteration = 2;
 
-    //using this while/file open to find dimensions of V/H nodes
-    else if (iteration == 2) {
-        // rewind(file);
-        while (1) {
-            if (fscanf(file, "%d(%d,%d)\n", &label, &x_val, &y_val) == 3) {
-                new_Block(label, x_val, y_val, iteration);
-                dim_list(label, x_val, y_val);
-            } 
-            else if (fscanf(file, "%c\n", &label_VH) == 1) {
-                new_VH(label_VH, iteration);
-                //find the most recently processed VH node with this label
-                Point* latest_vh = NULL;
-                for (int j = arr_index2 - 1; j >= 0; j--) {  //search backwards
-                    if (arr[j]->label == label_VH) {
-                        latest_vh = arr[j];
-                        break;  //take only the most recent one
-                    }
+    while (1) {
+        if (fscanf(in_file, "%d(%d,%d)\n", &label, &x_val, &y_val) == 3) {
+            new_Block(label, x_val, y_val, iteration);
+            dim_list_To_File(label, x_val, y_val, out_file2);
+        } 
+        else if (fscanf(in_file, "%c\n", &label_VH) == 1) {
+            new_VH(label_VH, iteration);
+            Point* latest_vh = NULL;
+            for (int j = arr_index2 - 1; j >= 0; j--) {
+                if (arr[j]->label == label_VH) {
+                    latest_vh = arr[j];
+                    break;
                 }
-                
-                if (latest_vh != NULL) {
-                    dim_list(latest_vh->label, latest_vh->x, latest_vh->y);
-                    latest_vh->label = 0;  //clearing it so it skips this one next time
-                }
-            } 
-            else 
-            {
-                //if neither format matched
-                break;
             }
-        }
-    }
-    
-    //PART 3
-    else if (iteration == 3) {
-        // rewind(file);
-        while (1) {
-            if (fscanf(file, "%d(%d,%d)\n", &label, &x_val, &y_val) == 3) {
-                new_Block(label, x_val, y_val, iteration);
-            } 
-            else if (fscanf(file, "%c\n", &label_VH) == 1) {
-                new_VH(label_VH, iteration);
-            } 
             
-            else 
-            {
-                //if neither format matched
-                break;
+            if (latest_vh != NULL) {
+                dim_list_To_File(latest_vh->label, latest_vh->x, latest_vh->y, out_file2);
+                latest_vh->label = 0;
             }
+        } 
+        else {
+            break;
         }
-        //copt of arr_index3 before using if needed, make sure to -1 if u wanna use
-        int arr_index3_copy = arr_index3;
-
-        //at this point tree is created
-        if ((arr_index3 > 0) && (iteration == 3)) {
-            //start printing from the last added element (root of the tree)
-            //doing -1 cuz i did i++ at end of each iteration in add_to_tree
-            //print_Tree((Point*)arr[arr_index3 - 1]);  // Start printing from the last added element (root of the tree)
-        }    
-        int coords = 0;
-        coords = packaging_right_side((Point*)arr[arr_index3 - 1], 0, 0); //start working from the last added element (root of the tree)
-
-
-
-
-
-
-        }
-
-    else 
-    {
-        fclose(file);
     }
-    
-    fclose(file);
 
-    return 0;
+    //PART 3-----------------> unfinished
+    //rewind(in_file);
+    //iteration = 3;
+    // while (1) {
+    //         if (fscanf(in_file, "%d(%d,%d)\n", &label, &x_val, &y_val) == 3) {
+    //             new_Block(label, x_val, y_val, iteration);
+    //         } 
+    //         else if (fscanf(in_file, "%c\n", &label_VH) == 1) {
+    //             new_VH(label_VH, iteration);
+    //         }     
+    //         else 
+    //         {
+    //             //if neither format matched
+    //             break;
+    //         }
+    //     }
+    //     //copt of arr_index3 before using if needed, make sure to -1 if u wanna use
+    //     int arr_index3_copy = arr_index3;
+    //     //at this point tree is created
+    //     if ((arr_index3 > 0) && (iteration == 3)) {
+    //         //start printing from the last added element (root of the tree)
+    //         //doing -1 cuz i did i++ at end of each iteration in add_to_tree
+    //         //print_Tree((Point*)arr[arr_index3 - 1]);  // Start printing from the last added element (root of the tree)
+    //     }    
+    //     int coords = 0;
+    //     packaging_right_side((Point*)arr[arr_index3 - 1], 0, 0); //start working from the last added element (root of the tree)
+    //     }
+
+    //close files
+    fclose(in_file);
+    fclose(out_file1);
+    fclose(out_file2);
+    fclose(out_file3);
+
+    return EXIT_SUCCESS;
 }
